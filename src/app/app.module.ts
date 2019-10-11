dcd322050ec76ec484291716757292240ae20f62
adrian <fak.adrian@gmail.com>
2019-10-11 16:19:47 +0200

    add project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { ListComponent } from './home/list/list.component';
+import { CardComponent } from './home/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
